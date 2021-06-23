7a47494f385bdcd25368214a305d73f3c801a2d3
harithar1234 <86344120+harithar1234@users.noreply.github.com>
2021-06-23 18:07:13 +0530

    Add files via upload

---
+++ Assignment 1/assignment1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+prob_E= 0.6
+prob_F=0.3
+prob_EF =0.2
+
+print("P(E)P(F) =", prob_E*prob_F)
+print("which is not equal to P(EF)=",prob_EF)
+print("so events E and F are not independent")
